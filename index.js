c18d0930398a9a7dd0264c502ffbc9346bac13a1
valkim55 <vesper5521@gmail.com>
2022-10-06 23:35:30 -0400

    prompt user for info

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Employee = require('./lib/Employee');
 
-
+// now that all employee objects are in place we can prompt the user for information to create an instance of employee object
+// once we get to role prompt, we will check for the type of role and place the object into the corresponding html section
 const collectInfo = function() {
     const employee = new Employee();
 
@@ -21,30 +22,30 @@ const collectInfo = function() {
             message: 'What is your name?'
         }
     ).then(({ name }) => {
-        this.employee.getName(name);
-        console.log(this.employee.name);
+        this.name = name; //  getName etc. will be used to feed the data to HTML later
+        console.info(name);
 
         inquirer.prompt(
             {
                 type: 'input',
                 name: 'email',
-                message: 'Please enter your email'
+                message: 'Please enter your email address'
             }
         ).then(({ email }) => {
-            this.employee.getEmail(email);
-            console.log(this.employee.email);
+            this.email = email;
+            console.log(email);
 
             inquirer.prompt(
                 {
-                    type: 'input',
+                    type: 'checkbox',
                     name: 'role',
                     message: 'What is your occupation?',
                     choices: ['Manager', 'Engineer', 'Intern']
                 }
             ).then(({role}) => {
-
-                this.employee.getRole(role);
-                console.log(this.employee.role)
+                this.role = role;
+                console.log(role);
+                console.log(this.employee);
             })
             
         })
@@ -54,5 +55,5 @@ const collectInfo = function() {
         
 }
 
-
-collectInfo();
+collectInfo(Employee);
+//console.log(Employee);
